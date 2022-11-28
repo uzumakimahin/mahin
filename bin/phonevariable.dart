54a0c534bc049ac4ce7d3d17eecd4608cd08ca95
token8300 <psmahin42@gmail.com>
2022-11-28 14:56:40 +0530

    commit

---
+++ bin/phonevariable.dart
@@ -0,0 +1,14 @@
+void main() {
+  String phonename = "poco"; //variable
+  int ram =8;
+  double model = 8.5;
+  String modelname = 'm4pro';
+  int rom =256;
+
+  print("my PHONE Details"); // direct string printing
+  print("phonename:$phonename"); // string intercolation-> ${}
+  print("RAM:$ram");
+  print("model:$model");
+  print("modelname:$modelname");
+  print("rom:$rom");
+}
---
+++ bin/variablepro.dart
@@ -0,0 +1,22 @@
+import 'package:test/expect.dart';
+
+void main(){
+  String name ="mahin"; //variable
+  int age =22;
+  double mark =8.5;
+  String email ='psmahin42@gmail.com';
+  int phone =9895997193;
+
+  print("my details");// direct string printing
+  print("name:$name");// string intercolation-> ${}
+  print("age:$age");
+  print("mark:$mark");
+  print("email:$email");
+  print("phone:$email");
+}
+/// i want inthe given format
+/// my detail
+/// name: mahin
+/// age:22
+/// mark:8.5
+/// etc..
