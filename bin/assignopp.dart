619e0197e39e4a0c20bbb46bccd7a3561fe628e1
token8300 <psmahin42@gmail.com>
2022-11-29 21:14:34 +0530

    commit

---
+++ bin/assignopp.dart
@@ -0,0 +1,14 @@
+void main(){
+
+dynamic a=10, b=30;
+print(a=b);
+print(a+=b);
+print(a+=b);
+print(a=b);
+print(a-=b);
+print(a=10);
+print(a-=b);
+print(a/=b);
+print(a~/=b);
+print(a%=b);
+}
---
+++ bin/relayop.dart
@@ -0,0 +1,16 @@
+void main(){
+  int age=18;
+  print(age > 18);
+  print(age < 18);
+  print(age >=18);
+  print(age <=18);
+  print(age ==18);
+  print(age !=18);
+
+
+
+
+
+
+
+}
