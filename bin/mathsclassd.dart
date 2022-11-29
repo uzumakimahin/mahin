b6cbd95418568ceec0cf4e391eba697302d29623
token8300 <psmahin42@gmail.com>
2022-11-29 13:44:23 +0530

    commit

---
+++ bin/mathsclassd.dart
@@ -0,0 +1,38 @@
+class Mathss{
+  void add() {
+    int a = 40,
+        b = 20,
+        sum = (a + b);
+    print('sum=$sum');
+  }
+
+  void sub() {
+    int a = 20,
+        b = 25,
+        subtract = (a - b);
+    print('subtract=$subtract');
+  }
+
+  void mul() {
+    int a = 10,
+        b = 3,
+        mul = (a * b);
+    print('mul=$mul');
+  }
+
+  void div() {
+    int a = 10,
+        b = 3,
+        div = (a ~/ b);
+    print('div=$div');
+  }
+
+
+}
+void main() {
+  Mathss obj = Mathss();
+  obj.add();
+  obj.sub();
+  obj.mul();
+  obj.div();
+}
