3334ea81e440b834bc3a35ec0f86e812e8b15a7c
token8300 <psmahin42@gmail.com>
2022-11-29 20:37:34 +0530

    commit

---
+++ bin/aritha.dart
@@ -0,0 +1,12 @@
+void main(){
+  var a= 10;
+   var b=20;
+  print('a+b     =${a+b}');
+  print('a-b     =${a-b}');
+  print('a*b     =${a*b}');
+  print('a/b     =${a~/b}');
+  print('a/b     =${a/b}');
+  print('b-a     =${b-a}');
+  print('a%b     =${a%b}');
+  print('-(a+b)  =${-(a+b)}');
+}
