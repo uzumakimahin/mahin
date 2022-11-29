cc1c02d970953f490d8758e828b3983942518a21
token8300 <psmahin42@gmail.com>
2022-11-29 13:19:33 +0530

    commit

---
+++ bin/clasloc.dart
@@ -0,0 +1,37 @@
+class Maths {
+  void add() {
+    int a = 40,
+        b = 20,
+        sum = (a + b);
+    print('sum=$sum');
+  }
+
+  void sub() {
+    int a = 20,
+        b = 25,
+        subtract = (a - b);
+    print('subtract=$subtract');
+  }
+
+  void mul() {
+    int a = 10,
+        b = 3,
+        mul = (a * b);
+    print('mul=$mul');
+  }
+
+  void div() {
+    int a = 10,
+        b = 3,
+        div = (a ~/ b);
+    print('div=$div');
+  }
+}
+void main(){
+  Maths obj = Maths();
+  obj.add();
+  obj.sub();
+  obj.mul();
+  obj.div();
+
+}
