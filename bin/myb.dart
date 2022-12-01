9a011b20adc627953be926e36e11c05dc862fba3
token8300 <psmahin42@gmail.com>
2022-12-01 16:38:15 +0530

    commit

---
+++ bin/myb.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+void main(){
+  print("enter ur name");
+  String name =stdin.readLineSync()!;
+  print("enter ur mark");
+  int mark=int.parse(stdin.readLineSync()!);
+  if(mark>=40) {
+    print("passed");
+    if (mark >= 80) {
+      print("pass with distinction");
+    }
+    else {
+      print("first class");
+    }
+  }
+  else{print("failed");}
+
+}
