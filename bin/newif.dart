e5e768795905c7ef6bfdbebe482681e08a906e42
token8300 <psmahin42@gmail.com>
2022-12-01 15:28:36 +0530

    commit

---
+++ bin/newif.dart
@@ -0,0 +1,17 @@
+void main(){
+
+    var uname= "mahin";
+    var password= "mahin999";
+if(uname=="mahin" && password=='mahin999') {
+  if (password.length >= 9) {
+    print("login sucess");
+  }
+  else {
+    print("password check");
+  }
+}
+   else {
+  print("login failed");
+}
+
+}
