2d9b6576dd68ce6e9a2f316dc48d52e21536e66a
token8300 <psmahin42@gmail.com>
2022-12-06 20:50:35 +0530

    commit

--- bin/function/returntype.dart
+++ bin/function/returntype.dart
@@ -1,14 +1,18 @@
 void main() {
-  print(add());
+  var result=add();
+  print(result);
   print(sub());
   print(mul());
   print(div());
+  print(show("mahin", 23));
+  print(func(1,name: "mahin",age: 23));
+  print(fun1(2, name:"beema",age: 24));
 }
 /// default function with return type
 int add(){
  int sum=1+2+4+7;
-// print(sum);
-  return sum;
+ print(sum);
+  return 600;
 }
 int sub(){
   int sut=7-6;
@@ -22,3 +26,21 @@ int div(){
   int divi= 4~/2;
   return divi;
 }
+///parameterised function with return type
+String show(String name,int age){
+  print("my name is $name");
+  return 'iam $age yrs old';
+}
+///optional parameterised function with return type
+dynamic func(int a,{String ?name,int ?age}){
+  print("si no $a" );
+  print("name:$name");
+  return"age:$age";
+}
+///optional named parameterised function with return type
+dynamic fun1(int a,{required String ?name,int ?age}){
+  print("si no:$a");
+  print("age:$age");
+  return "$name";
+}
+
