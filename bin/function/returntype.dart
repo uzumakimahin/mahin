4511d0d43bdb9227ad60030c514823004093a669
token8300 <psmahin42@gmail.com>
2022-12-06 19:57:00 +0530

    commit

---
+++ bin/function/returntype.dart
@@ -0,0 +1,24 @@
+void main() {
+  print(add());
+  print(sub());
+  print(mul());
+  print(div());
+}
+/// default function with return type
+int add(){
+ int sum=1+2+4+7;
+// print(sum);
+  return sum;
+}
+int sub(){
+  int sut=7-6;
+  return sut;
+}
+int mul(){
+  int mult =8*4;
+  return mult;
+}
+int div(){
+  int divi= 4~/2;
+  return divi;
+}
