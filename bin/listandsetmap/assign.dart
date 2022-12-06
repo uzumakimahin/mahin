bb810fb17e911ae5b16245189759d4e0c68bdd00
token8300 <psmahin42@gmail.com>
2022-12-06 19:17:36 +0530

    commit

---
+++ bin/listandsetmap/assign.dart
@@ -0,0 +1,13 @@
+void main(){
+ var list1=List.filled(1,"A");
+print(list1);
+ var list2=List.filled(1,"A,B");
+ print(list2);
+var list3=List.filled(1,"A,B,C");
+ print(list3);
+ var list4=List.filled(1,"A,B,C,D");
+ print(list4);
+ var list5=List.filled(1,"A,B,C,D,E");
+ print(list5);
+}
+
--- bin/listandsetmap/empty2.dart
+++ bin/listandsetmap/empty2.dart
@@ -17,7 +17,19 @@ list4.add(7);
 list4[5]=7;
 
  print(list4);
- var list5=List.filled(5, 6,);
+ var list5=List.filled(5, "A",);
 print(list5);
 
+  var list6 =List.from(list1,growable: true);
+  list6.add(98);
+print(list6);
+/// list.generate()
+var list7=List.generate(10,(index) =>index*3);
+print(list7);
+//list.of()
+var list8=List.of(list7);
+list8[9]=10;
+print(list8);
+var list9 =List.unmodifiable(list4);
+print(list9);
 }
